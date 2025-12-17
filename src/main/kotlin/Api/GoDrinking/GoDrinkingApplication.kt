08c0c97d9290cb6a20dc952d2e14acfde1ae45c1
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2025-12-17 10:19:03 -0300

    Spring Boot Inicializer Project

---
+++ src/main/kotlin/Api/GoDrinking/GoDrinkingApplication.kt
@@ -0,0 +1,11 @@
+package Api.GoDrinking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoDrinkingApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoDrinkingApplication>(*args)
+}
---
+++ src/test/kotlin/Api/GoDrinking/GoDrinkingApplicationTests.kt
@@ -0,0 +1,13 @@
+package Api.GoDrinking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoDrinkingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
