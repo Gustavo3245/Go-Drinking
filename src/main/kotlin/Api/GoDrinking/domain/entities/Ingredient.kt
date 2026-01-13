5797fa3bd68967aebbb7dfcd094e554d51425e0b
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2026-01-13 20:36:07 -0300

    Feat: (domain) introducing the ability to negate special characters

--- src/main/kotlin/Api/GoDrinking/domain/entities/Ingredient.kt
+++ src/main/kotlin/Api/GoDrinking/domain/entities/Ingredient.kt
@@ -1,9 +1,10 @@
 package Api.GoDrinking.domain.entities
 
+import Api.GoDrinking.domain.value_objects.Name
 import java.util.UUID
 
 data class Ingredient(
     val id: UUID,
-    val name: String,
+    val name: Name,
     val category: String
 )
--- src/main/kotlin/Api/GoDrinking/domain/errors/NameError.kt
+++ src/main/kotlin/Api/GoDrinking/domain/errors/NameError.kt
@@ -3,4 +3,5 @@ package Api.GoDrinking.domain.errors
 sealed class NameError(message: String): DomainError(message) {
     data object Empty: NameError("name should't not be empty")
     data object TooLong: NameError("name should't not be that long (50 characters)")
+    data object InvalidCharacters: NameError("name must not contain these invalid characters")
 }
