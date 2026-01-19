6f89fc67735606812350482cd3ab9008e2ec4c50
Gustavo3245 <gustavomoreiradev3245@gmail.com>
2026-01-18 22:21:46 -0300

    Feat: (database) implementing group_members postgres table

---
+++ src/main/kotlin/Api/db/tables/06_create_group_members.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "group_members" (
+    user_id UUID NOT NULL,
+    group_id UUID NOT NULL,
+    positions member_position DEFAULT 'Member',
+
+    joined_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY "user_id" REFERENCES "user" ("id") ON DELETE CASCADE,
+    FOREIGN KEY "group_id" REFERENCES "group" ("id") ON DELETE CASCADE,
+    PRIMARY KEY(user_id, group_id)
+);
