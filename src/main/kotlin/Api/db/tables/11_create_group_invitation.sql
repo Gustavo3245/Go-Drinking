1cdf210ede7cd5985a7f1dc70c3fba41c42acd86
Gustavo3245 <gustavomoreiradev3245@gmail.com>
2026-01-19 22:31:06 -0300

    Feat: (domain) created invitation group table for individual invitations

---
+++ src/main/kotlin/Api/db/tables/11_create_group_invitation.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS "group_invitation" (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    group_id UUID NOT NULL,
+    sender_id UUID NOT NULL,
+    recipient_id UUID NOT NULL,
+
+    status invitation_status DEFAULT 'Pending',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("group_id") REFERENCES "group" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("sender_id") REFERENCES "user" ("id"),
+    FOREIGN KEY ("recipient_id") REFERENCES "user" ("id"),
+
+    CONSTRAINT check_not_self_invite CHECK (sender_id <> recipient_id)
+);
